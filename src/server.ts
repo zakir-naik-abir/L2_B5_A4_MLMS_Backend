233736e8c3b1353f00a895e0835e80d838d00053
Zakir Hasan <zakirhasanabir@gmail.com>
2025-07-01 23:54:26 +0600

    adder config and connect server and mongodb

---
+++ src/server.ts
@@ -0,0 +1,32 @@
+import express, { Application, Request, Response } from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import config from "./config";
+
+const app: Application = express();
+
+// middlewares
+app.use(cors());
+app.use(express.json());
+
+
+// server
+app.get("/", (req, res) => {
+  res.send({ success: true, message: "Minimal Library Management System" });
+});
+
+app.listen(config.port, () => {
+  console.log(`✅ Server is running on port ${config.port}`);
+});
+
+// mongo database
+async function server() {
+  try {
+    await mongoose.connect(config.database_url!);
+    console.log(`✅ Connected to Mongo Database`);
+  } catch (error) {
+    console.log(`❌ Server error ${server}`);
+  }
+}
+
+server();
